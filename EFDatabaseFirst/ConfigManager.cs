29c6c164efa889e38af71a993c7da61bdb3416f2
Cliv-create <66003937+Cliv-create@users.noreply.github.com>
2025-03-19 16:00:39 +0200

    Add project files.

---
+++ EFDatabaseFirst/ConfigManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EFDatabaseFirst
+{
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(Dictionary<string, string>))]
+    internal partial class ConfigJsonContext : JsonSerializerContext
+    {
+    }
+
+    internal static class ConfigManager
+    {
+        private static readonly string ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+        private static Dictionary<string, string> _config;
+
+        /// <summary>
+        /// Loads config.json from folder, containing executable. config.json gets deserialized in Dictionary<string, string>. To get config values use Get property.
+        /// </summary>
+        /// <exception cref="Exception">Exception if config.json was not found.</exception>
+        public static void LoadConfig()
+        {
+            Console.WriteLine("Loading config");
+            if (File.Exists(ConfigFilePath))
+            {
+                Console.WriteLine("Found file!");
+                string json = File.ReadAllText(ConfigFilePath);
+                Console.WriteLine("Read file contents!");
+                _config = JsonSerializer.Deserialize(json, ConfigJsonContext.Default.DictionaryStringString);
+                Console.WriteLine($"Deserialized contents successfully!");
+            }
+            else
+            {
+                throw new Exception("config.json not found!");
+                // _config = new Dictionary<string, string>();
+            }
+        }
+
+        /// <summary>
+        /// Generates config.json in folder, containing executable. Config will have all the settings field. Mandatory settings: token, yt-dlp_path.
+        /// </summary>
+        public static void GenerateInitialConfig()
+        {
+            Console.WriteLine($"Writing initial config...");
+            _config = new Dictionary<string, string>();
+            _config.Add("Server", "SERVER_NAME_HERE");
+            _config.Add("Database", "DATABASE_NAME_HERE");
+            _config.Add("TrustedConnection", "TRUSTED_CONNECTION_BOOL_HERE");
+            _config.Add("TrustServerCertificate", "TRUST_SERVER_CERTIFICATE_BOOL_HERE");
+            string json = JsonSerializer.Serialize(_config, ConfigJsonContext.Default.DictionaryStringString);
+            File.WriteAllText(ConfigFilePath, json);
+            Console.WriteLine($"Successfully wrote initial config!");
+        }
+
+        /// <summary>
+        /// Uses TryGetValue to get value from config.json. Can generate exception.
+        /// </summary>
+        /// <param name="key">Dictionary key.</param>
+        /// <param name="defaultValue">null by default. If set, will return if nothning was found.</param>
+        /// <returns>Found value, or null (defaultValue if set) if nothning was found.</returns>
+        public static string Get(string key, string defaultValue = null)
+        {
+            return _config.TryGetValue(key, out string value) ? value : defaultValue;
+        }
+
+        // Config is meant to be defined by user manually.
+        // It is not meant to be changed in runtime, but to be changed manually.
+        // TODO: If runtime config change capabilities will be added - uncomment this code.
+
+        /*
+        public static void SaveConfig()
+        {
+            string json = JsonSerializer.Serialize(_config, ConfigJsonContext.Default.DictionaryStringString);
+            File.WriteAllText(ConfigFilePath, json);
+        }
+        */
+
+        /*
+        public static void Set(string key, string value)
+        {
+            _config[key] = value;
+            SaveConfig();
+        }
+        */
+    }
+}
---
+++ EFDatabaseFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EFDatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (System.IO.File.Exists("config.json"))
+                {
+                    ConfigManager.LoadConfig();
+                }
+                else
+                {
+                    ConfigManager.GenerateInitialConfig();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
